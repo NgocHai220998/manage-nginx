3acd4f38eef09c9e101c15f8a9dc1c67ea64a5a9
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-11-25 12:21:43 +0700

    Update Home page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Home page!!!");
+  res.send("Home page!!! Update ne");
 });
 
 app.get("/admin", (req, res) => {
